13418e41a25590989e9734d4d346f84c55cbd641
Tushar Anand <tusharranand@outlook.com>
2024-11-03 22:38:34 +0530

    Completed Adapter Design Pattern

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,2 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Structural.Adapter;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int choice = default;
+            do
+            {
+                Console.WriteLine("Enter a number to test related Design Pattern");
+                Console.WriteLine("1. Behavioral Patterns");
+                Console.WriteLine("2. Creational Patterns");
+                Console.WriteLine("3. Structural Patterns");
+                Console.WriteLine("4. EXIT");
+                try
+                {
+                    Console.Write("\nEnter your choice here: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Start Behavioral Patterns");
+                            StartBehavioralTesting();
+                            break;
+                        case 2:
+                            Console.WriteLine("Start Creational Patterns");
+                            StartCreationalTesting();
+                            break;
+                        case 3:
+                            Console.WriteLine("Start Structural Patterns");
+                            StartStructuralTesting();
+                            break;
+                        case 4:
+                            Console.WriteLine("Exiting Program.");
+                            break;
+                        default:
+                            Console.WriteLine("Please enter a valid input.\n");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("");
+                }
+            }
+            while (choice != 4);
+        }
+
+        private static void StartStructuralTesting()
+        {
+            int choice = default;
+            do
+            {
+                Console.WriteLine("Enter a number to test related Structural Design Pattern");
+                Console.WriteLine("1. Adpter");
+                Console.WriteLine("2. Bridge");
+                Console.WriteLine("3. Composite");
+                Console.WriteLine("4. Decorator");
+                Console.WriteLine("5. Facade");
+                Console.WriteLine("6. Flyweight");
+                Console.WriteLine("7. Proxy");
+                Console.WriteLine("8. EXIT");
+                try
+                {
+                    Console.Write("\nEnter yout choice here: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Starting Adapter\n");
+                            AdapterStartPoint.StartAdapterDemo();
+                            break;
+                        case 2:
+                            Console.WriteLine("Starting Bridge");
+                            break;
+                        case 3:
+                            Console.WriteLine("Starting Composite");
+                            break;
+                        case 4:
+                            Console.WriteLine("Starting Decorator");
+                            break;
+                        case 5:
+                            Console.WriteLine("Starting Facade");
+                            break;
+                        case 6:
+                            Console.WriteLine("Starting Flyweight");
+                            break;
+                        case 7:
+                            Console.WriteLine("Starting Proxy");
+                            break;
+                        case 8:
+                            Console.WriteLine("Exiting Sub Menu.");
+                            break;
+                        default:
+                            Console.WriteLine("Please enter on of the given options.");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please Enter a valid choice.");
+                }
+            } while (choice != 8);
+        }
+
+        private static void StartCreationalTesting()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void StartBehavioralTesting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Adapter/AdapterStartPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Adapter
+{
+    public static class AdapterStartPoint
+    {
+        public static void StartAdapterDemo()
+        {
+            Adaptee adaptee = new();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client.");
+            Console.WriteLine("But with adapter, client can call it's method.\nPress any key to proceed...");
+            _ = Console.ReadKey();
+
+            Console.WriteLine(target.GetRequest());
+
+            Console.Write("\nTry Usage Example? (y/n): ");
+            string example = Console.ReadLine();
+            if (!string.IsNullOrEmpty(example) && example.ToLower() == "y")
+            {
+                Console.WriteLine("Client sends XML/CSV data but the API expects JSON.");
+                string inputXML = "\"This is an XML string\"";
+                Console.WriteLine($"Sending dummy XML input.{inputXML}\nPress any key to proceed...");
+                _ = Console.ReadKey();
+
+                _ = target.InputXMLIntermediateMethod(inputXML);
+
+                string inputCSV = "\"This is a CSV string\"";
+                Console.WriteLine($"Sending dummy CSV input.{inputCSV}\nPress any key to proceed...");
+                _ = Console.ReadKey();
+
+                _ = target.InputCSVIntermediateMethod(inputCSV);
+
+            }
+        }
+    }
+}
