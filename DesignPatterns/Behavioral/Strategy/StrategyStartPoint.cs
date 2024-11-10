de40bbc9a60361a2854946a32e4674e4642065cf
Tushar Anand <tusharranand@outlook.com>
2024-11-10 19:13:29 +0530

    Added Strategy design pattern

---
+++ DesignPatterns/Behavioral/Strategy/StrategyStartPoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Behavioral.Strategy
+{
+    public class StrategyStartPoint
+    {
+        public static void StartStrategyDemo()
+        {
+            Context context = new();
+
+            Console.WriteLine("Client says: Strategy is set to normal sorting. Press any key to proceed...");
+            _ = Console.ReadKey();
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine("Client says: Strategy is set to reverse sorting. Press any key to proceed...");
+            _ = Console.ReadKey();
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+    }
+}
